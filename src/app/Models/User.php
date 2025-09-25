c8f08cac9fdab7b1ea0b887e58bbda7799eb8944
otake taro <tarootake75@gmail.com>
2025-09-25 19:13:53 +0900

    chore(user): remove HasApiTokens remnants

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -5,13 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-// use Laravel\Sanctum\HasApiTokens; // APIを使うなら有効化
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable; // , HasApiTokens;
+    use HasFactory, Notifiable; 
 
     protected $fillable = [
         'name',
